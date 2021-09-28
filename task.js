0a2fab1184e84700f5d89c17bdb699957ecff98e
Aland <32212842+alandio@users.noreply.github.com>
2021-09-28 20:53:00 +0000

    Add the ability to sort a local file
    Fix a bug in the the nested for loops, and add the ability to sort a local file.

--- task.js
+++ task.js
@@ -1,44 +1,51 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+const numbers = [ 5, 8, 0, 1, 9, 11, 15, 16]
+let mutableList = numbers
+let fileContent
 
 console.log("Original numbers list: ", numbers)
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] > numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
+function ascendingSort(list) {
+    for (let i = 0; i < list.length; i++) {
+        // replace `list.length - 1` with `list.length - i` in order to reduce the cpu cycles
+        for (let j = 0; j < list.length - i; j++) {
+            // replace i with j to sort correctly
+            if (list[j] > list[j + 1]) {
+                let temp = list[j]
+                list[j] = list[j + 1]
+                list[j + 1] = temp
+            }
+        }
+    }
+    return list
+}
+
+console.log("Sorted list: ", ascendingSort(mutableList))
+
+for (let i = 0; i < mutableList.length; i++) {
+        // replace `list.length - 1` with `list.length - i` in order to reduce the cpu cycles
+        for (let j = 0; j < mutableList.length - i; j++) {
+            // replace i with j to sort correctly
+            if (mutableList[j] < mutableList[j + 1]) {
+                let temp = mutableList[j]
+                mutableList[j] = mutableList[j + 1]
+                mutableList[j + 1] = temp
         }
     }
 }
 
-console.log("Numbers list After sorting: ", numbers)
+console.log("Numbers list After Desc sorting: ", mutableList)
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
+// read the data.txt file from file system and sort it
+const fs = require('fs')
+
+try {
+    fileContent = fs.readFileSync('data.txt', 'utf8')
+} catch (err) {
+    console.error(err)
 }
 
-console.log("Numbers list After Desc sorting: ", numbers)
+let sortedData = ascendingSort(fileContent.split(',').map(Number)).join(",")
 
-
+fs.writeFile('output.txt', sortedData, (err) => {
+    if (err) return console.log(err)
+});
